4fc1997b8df2babb4ab508faf501108f3a7aecb6
Mumtahina Mimu <mumtahinamimu.web@gmail.com>
2021-09-21 19:41:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+/text change/
+function textChange(){
+
+document.getElementById('h2').innerHTML="Welcome to My Website";
+
+}
+
+function textChan(){
+
+  document.getElementsByClassName('h2')[2].innerHTML="Welcome to My Website";
+  
+  }
+
+  function dateChange(){
+
+    document.getElementById('date').innerHTML= Date();
+    
+    }
+
+    function colorChange(){
+
+      document.getElementById('color').style.color= "red";
+      
+      }
+
+      function fontSizeChange(){
+
+        document.getElementById('font_size').style.fontSize= "60px";
+        
+        }
+
+      function border(){
+
+        document.getElementById('border').style.border= "3px solid green";
+        
+        }
